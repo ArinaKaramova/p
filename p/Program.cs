31a513a34dc8b980cb069cf92320d1480b5257fa
ArinaKaramova <karamowaarina@yandex.ru>
2021-10-28 18:25:38 +0500

    hm4

--- p/Program.cs
+++ p/Program.cs
@@ -6,7 +6,13 @@ namespace p
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var x = XY(3, 11) + XY(5, 13) + XY(7, 17);
+            Console.WriteLine(Math.Round(x, 3));
+        }
+        static double XY(double n1, double n2)
+        {
+            return (1 + Math.Pow(n1, 2)) / (1 + Math.Pow(n2, 2));
         }
     }
 }
