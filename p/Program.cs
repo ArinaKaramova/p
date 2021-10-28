c95a39441a0b5e9e5ddd19c9c2938573541e4cda
ArinaKaramova <karamowaarina@yandex.ru>
2021-10-28 17:46:46 +0500

    hm2

--- p/Program.cs
+++ p/Program.cs
@@ -6,7 +6,18 @@ namespace p
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            double a, b, c, m;
+            Console.WriteLine("Введите первый катет");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второй катет");
+            b = double.Parse(Console.ReadLine());
+
+            c = Math.Sqrt(a * a + b * b);
+            m = c / 2;
+
+            Console.WriteLine("M = " + m);
         }
     }
 }
